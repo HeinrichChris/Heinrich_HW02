9944248b9f4b216b7bdb7011719f4cce8d57836f
Matthew Marine <marinemr@muohio.edu>
2012-09-25 22:08:49 -0400

    Code Review
    There was nothing majorly wrong.  The code worked, after all.  Although,
    I'd suggest that pointer names be made more descriptive, either telling
    what the reference is, what it does, or what it is used for.
    
    Also, there was no need to cast your red, green, blue colors a bunch of
    different times.  If you are drawing using uint8_t, then keep them
    uint8_t unless you absolutely need to change them into something else
    for some reason.
    
    It seemed like a cool project when I ran it!  Good job!

--- vc10/Square.cpp
+++ vc10/Square.cpp
@@ -5,13 +5,22 @@ using namespace ci;
 
 Square::Square()
 {
-	next=prev=this;
-	red = green = blue = 0;
-	x=y=size=0.0f;
+	next = this;
+	prev = this;
+
+	red = 0;
+	green = 0;
+	blue = 0;
+
+	x = 0.0f;
+	y = 0.0f;
+	size = 0.0f;
+
 	falls = true;
 	speed = 5;
 }
 
+// This method inserts a single Square after another square
 void insertAfter(Square* new_square, Square* insert_here){
 	new_square->next = insert_here->next;
 	new_square->prev = insert_here;
@@ -19,15 +28,25 @@ void insertAfter(Square* new_square, Square* insert_here){
 	insert_here->next = new_square;
 }
 
+// This method updates the instance of Square
+// You should probably shouldn't include collision detection here.  Put it in the HW02App.cpp update() instead
+// By putting collision detection here, you are calling a lot of unnecessary checks.  For example, you would only have to do
+// A,B A,C A,D
+// B,C, B,D
+// C,D
+// in a list of A...D instead of checking every one multiple times like what happens here
 void Square::update(){
 	if(falls == true)
 		this->y+=speed;
 	else
 		this->y-=speed;
 
+	// You might want to make a more descriptive name for your pointers in the future
+	// "that" says nothing about what the pointer is or what it is used for.
 	Square* that = this->next;
 
 	while(that!=this){
+		// if you saw this line out of context, could you tell me what this and that are?
 		collide(this,that);
 		that = that->next;
 	}
@@ -37,11 +56,13 @@ void Square::update(){
 	}
 }
 
+// This method draws the Square to the app window
 void Square::draw(){
-	gl::color(Color8u((uint8_t)red,(uint8_t)green,(uint8_t)blue));
+	gl::color(Color8u((uint8_t)red,(uint8_t)green,(uint8_t)blue)); // Why aren't your colors already uint8_t?
 	gl::drawSolidRect(Rectf(x,y,x+size,y+size));
 }
 
+// This method checks collisions and changes colors of the Squares if a collision occurs
 void collide(Square* square1, Square* square2){
 	if(square1->x < square2->x+square2->size){
 		if(square1->x >= square2->x){
@@ -55,6 +76,7 @@ void collide(Square* square1, Square* square2){
 						square1->red = 255;
 						square1->green = 0;
 						square1->blue = 0;
+
 						square2->red = 255;
 						square2->green = 0;
 						square2->blue = 0;
@@ -63,6 +85,7 @@ void collide(Square* square1, Square* square2){
 						square1->red = 0;
 						square1->green = 255;
 						square1->blue = 0;
+
 						square2->red = 0;
 						square2->green = 255;
 						square2->blue = 0;
@@ -71,6 +94,7 @@ void collide(Square* square1, Square* square2){
 						square1->red = 0;
 						square1->green = 0;
 						square1->blue = 255;
+
 						square2->red = 0;
 						square2->green = 0;
 						square2->blue = 255;
@@ -81,6 +105,8 @@ void collide(Square* square1, Square* square2){
 	}
 }
 
+// This method removes the Square from the Linked List that it is in.
+// This does not delete the Square
 void Square::remove(){
 	this->prev->next = this->next;
 	this->next->prev = this->prev;
